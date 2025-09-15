fd96507e1f51c2c84323fe47eb664799bae5df36
zamanjabali <jabalizaman04@gmail.com>
2025-09-15 23:40:55 +0530

    first commit

---
+++ grade.js
@@ -0,0 +1,12 @@
+let marks = 50;
+
+if (marks >= 90) {
+    console.log("A");
+}else if (marks >=80 ) {
+    console.log("B");
+}else if (marks >= 70) {
+    console.log("C");
+}
+else{
+    console.log("fail");
+}
