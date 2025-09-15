5f0a81e0d33ddb03af55c2578865cee255c73f1e
zamanjabali <jabalizaman04@gmail.com>
2025-09-15 21:58:25 +0530

    first commit

---
+++ tables.js
@@ -0,0 +1,8 @@
+for (let n=1; n<=10; n++){
+    console.log("printing the multiples of "+n);
+    for(let i=1; i<=10; i++){
+        let result = n*i;
+        console.log(n+" * "+i+" = "+result);
+    }
+    console.log();
+    }
