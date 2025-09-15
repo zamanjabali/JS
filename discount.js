71c05c18154f10d8eb2b45dd9c105f5772145a98
zamanjabali <jabalizaman04@gmail.com>
2025-09-15 23:47:33 +0530

    first commit

---
+++ discount.js
@@ -0,0 +1,12 @@
+let amount = 8000;
+if (amount >5000) {
+console.log("Discount is 20%");
+}
+else if (amount > 3000) {
+    console.log("Discount is 15%");
+}
+else if (amount > 1000) {
+    console.log("Discount is 5%");
+} else{
+    console.log("No Discount");
+}
