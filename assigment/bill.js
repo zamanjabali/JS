96d2c2bdf17afa5f4ba55b2de53ec2dce8b4af0a
zamanjabali <jabalizaman04@gmail.com>
2025-09-15 21:26:21 +0530

    done

--- assigment/bill.js
+++ assigment/bill.js
@@ -1,10 +1,24 @@
 let name = "zaman"
 let cons_NO = "101"
-let units = "350"
+let units = "100"
 
 let amount = 0;
-if(amount<=100)
+if(amount<=100){
+    amount = units*1.5;
 
+}else if(units <=200){
+    amount = (100*1.5) + (units-100)*2.5;
+}
+else if(units <=400){
+    amount = (100*1.5) + (100*2.5) + (units-200)*3.5;
+}
+else{
+    amount = (100*1.5) + (100*2.5) + (200*3.5) + (units-400)*4;
+}
+console.log(`Name: ${name}`);
+console.log(`Consumer No: ${cons_NO}`);
+console.log(`Units Consumed: ${units}`);
+console.log(`Amount: ${amount}`);
 
 
 
