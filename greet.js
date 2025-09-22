cc1fc997c6f967a32c04021ba5b3171491c8bb70
zamanjabali <jabalizaman04@gmail.com>
2025-09-23 00:16:32 +0530

    first commit

---
+++ greet.js
@@ -0,0 +1,11 @@
+function greet(){
+    console.log("Hello, World!");
+}
+nodegreet();
+
+function nameGreet(name){
+    console.log("Hello  " + name +"");
+}
+nameGreet("zaman");
+nameGreet("ayyan");
+nameGreet("basit");
