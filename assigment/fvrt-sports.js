96e2164ec2c0ed8a11d815d0f48ef80537222d38
zamanjabali <jabalizaman04@gmail.com>
2025-09-28 23:31:32 +0530

    fvrt-sports

---
+++ assigment/fvrt-sports.js
@@ -0,0 +1,9 @@
+let sports = ["Football", "Cricket", "kabbadi", "volleyball", "throwball"];
+console.log("First sport:", sports[0]);
+console.log("Last sport:", sports[sports.length - 1]);
+
+sports.push("Hockey");
+console.log("After adding a sport at the end:", sports);
+
+sports.shift;
+console.log("After removing the first sport:", sports);
